89f8693b7a163800ad3a05590ccd104583e09471
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2024-11-17 22:08:47 +0500

    how to create custom pipes in Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { USDPKRPipe } from './pipes/usdpkr.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    USDPKRPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/usdpkr.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { USDPKRPipe } from './usdpkr.pipe';
+
+describe('USDPKRPipe', () => {
+  it('create an instance', () => {
+    const pipe = new USDPKRPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
